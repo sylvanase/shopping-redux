b917a346a3f44f1b488d4259d5b11a17900c98b6
sylvanas <jzm362466464@126.com>
2020-11-28 23:12:18 +0800

    init pro

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Product from './product'
+import Cart from './cart'
+
+function App() {
+	return (
+		<div>
+			<Product />
+			<Cart />
+		</div>
+	)
+}
+
+export default App
