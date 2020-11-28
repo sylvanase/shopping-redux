15e573d4a987868c129442215fead62531cc0bb9
sylvanas <jzm362466464@126.com>
2020-11-29 00:15:03 +0800

    complete pro

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,13 +1,23 @@
-/*
- * @Author: sylvanas
- * @Date: 2020-11-28 22:57:34
- * @LastEditors: sylvanas
- * @LastEditTime: 2020-11-28 23:10:32
- * @Description:
- */
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as cartActions from '../store/actions/cart.actions'
+
 class Cart extends Component {
+	componentDidMount() {
+		const { loadCarts } = this.props
+		// 向服务器端发送请求 获取购物车列表数据
+		loadCarts()
+	}
+	changeProductNumber(cid, event) {
+		const { changeServiceProductNumber } = this.props
+		// 获取商品的最新数量
+		const count = event.target.value
+		// 向服务器端发送请求 告诉服务器端我们要将哪一个商品的数量更改成什么
+		changeServiceProductNumber({ cid, count })
+	}
 	render() {
+		const { carts, deleteProductFromCart } = this.props
 		return (
 			<section className="container content-section">
 				<h2 className="section-header">购物车</h2>
@@ -17,54 +27,57 @@ class Cart extends Component {
 					<span className="cart-quantity cart-header cart-column">数量</span>
 				</div>
 				<div className="cart-items">
-					<div className="cart-row">
-						<div className="cart-item cart-column">
-							<img
-								className="cart-item-image"
-								src="images/01.webp"
-								width="100"
-								height="100"
-							/>
-							<span className="cart-item-title">
-								小户型简约现代网红双人三人客厅科技布免洗布艺
-							</span>
+					{carts.map((product) => (
+						<div className="cart-row" key={product.id}>
+							<div className="cart-item cart-column">
+								<img
+									className="cart-item-image"
+									src={`http://localhost:3005${product.thumbnail}`}
+									width="100"
+									height="100"
+									alt=""
+								/>
+								<span className="cart-item-title">{product.title}</span>
+							</div>
+							<span className="cart-price cart-column">￥{product.price}</span>
+							<div className="cart-quantity cart-column">
+								<input
+									className="cart-quantity-input"
+									type="number"
+									value={product.count}
+									onChange={(e) => this.changeProductNumber(product.id, e)}
+								/>
+								<button
+									onClick={() => deleteProductFromCart(product.id)}
+									className="btn btn-danger"
+									type="button"
+								>
+									删除
+								</button>
+							</div>
 						</div>
-						<span className="cart-price cart-column">￥1020</span>
-						<div className="cart-quantity cart-column">
-							<input className="cart-quantity-input" type="number" />
-							<button className="btn btn-danger" type="button">
-								删除
-							</button>
-						</div>
-					</div>
-					<div className="cart-row">
-						<div className="cart-item cart-column">
-							<img
-								className="cart-item-image"
-								src="images/02.webp"
-								width="100"
-								height="100"
-							/>
-							<span className="cart-item-title">
-								11全网通4G手机官方iPhonexr
-							</span>
-						</div>
-						<span className="cart-price cart-column">￥4758</span>
-						<div className="cart-quantity cart-column">
-							<input className="cart-quantity-input" type="number" />
-							<button className="btn btn-danger" type="button">
-								删除
-							</button>
-						</div>
-					</div>
+					))}
 				</div>
 				<div className="cart-total">
 					<strong className="cart-total-title">总价</strong>
-					<span className="cart-total-price">￥39.97</span>
+					<span className="cart-total-price">
+						￥
+						{carts.reduce((total, product) => {
+							return (total += product.count * product.price)
+						}, 0)}
+					</span>
 				</div>
 			</section>
 		)
 	}
 }
 
-export default Cart
+const mapStateToProps = (state) => ({
+	carts: state.carts,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+	...bindActionCreators(cartActions, dispatch),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
